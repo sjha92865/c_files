7b24213002f4d0b4513ab867a553ed6f88db57bc
Shubham Jha <71147870+sjha92865@users.noreply.github.com>
2022-01-25 17:44:54 +0530

    Update main (11).c

--- main (11).c
+++ main (11).c
@@ -70,3 +70,81 @@ int main()
 	}
 return 0;	
 }
+
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+//reverse a list
+typedef struct node
+{
+	int data;
+	struct node *link;
+}node;
+
+node *newNode(int data)
+{
+	node *p;
+	p = (node*)malloc(sizeof(node)); 
+	p->data = data;   // First node
+	p->link = NULL;
+	return p;  
+}
+
+int main()
+{
+	int len_loop;
+	node *p ,*temp,*root,*prev,*curr,*next;
+	p = newNode(1);
+	p->link = newNode(2);
+	p->link->link = newNode(3);
+	
+	p->link->link->link = newNode(4);
+	//p->next->next->next->next = newNode(5);
+	
+	/*
+	printf("%d",prev->data);
+	printf("%d",prev->next->data);
+	printf("%d",prev->next->next->data);
+	printf("%d",prev->next->next->next->data);
+	printf("%d",prev->next->next->next->next->data);
+	//printf("%d",prev->next->next->next->next->next->data);
+	*/
+	root=p;
+	//printf("%d \n",p->data);
+	prev=root;
+	curr=prev->link;
+	next=curr->link;
+	temp=next;
+	printf("%d  %d %d %d\n",temp->data,prev->data,curr->data,next->data);
+	while(temp->link!=NULL){
+	    //printf("%d ",temp->data);
+	    next->link=curr;
+	    printf("%d next\n",next->data);
+	    curr->link=prev;
+	    prev->link=NULL;
+	    temp=temp->link;
+	    printf("%d  %d %d %d\n",temp->data,prev->data,curr->data,next->data);
+	}
+	prev=curr;
+	curr=next;
+	next=temp;
+	
+	next->link=curr;
+	curr->link=prev;
+	prev->link=NULL;
+	root=next;
+	
+	
+	//printf("%d ",root->data);
+	temp=root;
+	while(temp!=NULL)
+	{
+	    printf("%d ",temp->data);
+	    temp=temp->link;
+	}
+	
+	
+return 0;	
+}
